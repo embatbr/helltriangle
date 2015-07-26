a00c4a7de3ba53fc4ac20dacec200566384c48ca
Eduardo Tenorio <embatbr@gmail.com>
2015-07-26 00:49:27 -0300

    Melhora considerável no código (correção de alguns bugs) e na documentação (README agora serve para algo).
    Signed-off-by: Eduardo Tenorio <embatbr@gmail.com>

--- README.md
+++ README.md
@@ -5,4 +5,31 @@ Implementação do desafio Hell Triangle, descrito no documento HellTriangle-Cha
 
 ## Considerações
 
-- A entrada é composta apenas por integers, embora com floats rode corretamente;
+- A entrada é composta apenas por inteiros, embora uma adaptação com decimais possa rodar se feita corretamente. Contudo, este caso foi deixado de lado por não parecer necessário.
+
+
+## Chamada
+
+O módulo é chamado através do terminal, no formato `python3.4 helltriangle.py <num_lines> <lim_min> <lim_max>`, onde
+
+- `num_lines` é o número de linhas do triângulo.
+- `lim_min` é o menor valor possível em uma posição.
+- `lim_max` é o maior valor possível em uma posição.
+
+A regra para uma entrada válida é `num_lines > 0` e `lim_min < lim_max`.
+
+
+## Solução
+
+A linguagem utilizada é Python 3.4, devido à minha familiaridade e preferência pessoal.
+
+A implementação foi feita de modo simples. Inicialmente pensei em utilizar arrays de NumPy, contudo concluí que seria uma solução "utilizar uma bazuca para matar uma mosca".
+
+Existem 2 funções auxiliares. A primeira, `create_example`, cria um triângulo exemplo dado o número de linhas (altura) e os limites para os valores (tipo `int`). Caso a regra descrita na seção anterior seja desobedecida, uma exception (InvalidNumLinesError ou InvalidLimitsError, dependendo da violação) é lançada. A segunda função é `print_result`, utilizada apenas para exibir o resultado de forma amigável.
+
+A função principal é `maximum_total`, que recebe o maldito triângulo (variável `example`), sua altura (`num_lines`) e o caminho e resultado iniciais. A função executa uma varredura em profundidade (não é possível, do ponto de vista do algoritmo, antecipar os valores futuros). O triângulo é tratado como uma matriz com valores acima da diagonal principal desconsiderados. Embora seja visto como uma matriz, não é modelado (em termo de estrutura de dados) como uma. Apenas é utilizada uma lista de listas de inteiros. Para cada linha `i`, o valor `j` pode ser `j_prev` ou `j_prev + 1` (indica a coluna à direita daquela indicada por `j_prev`). Isso evita que a busca seja "aberta" demais, algo consideravelmente custoso para triângulos muito altos. Em caso de empate, a regra é "descer tem prioridade sobre descer e pegar a direita" (`j_prev` > `j_prev + 1`).
+
+
+## Testes
+
+TODO amanhã
--- helltriangle.py
+++ helltriangle.py
@@ -7,41 +7,119 @@ or user inputs),inside the code block "if __name__ == '__main__'" at the end of
 the file.
 
 TODO try 2 solutions: using lists and using NumPy.
-TODO Use the "time" module (if that's it's real name) to check the execution time.
 """
 
 
-def find_result(example, size, path, result):
-    """In case of a tie, the pos "down" has preference over "downright".
+import random as rd
+
+
+class InvalidNumLinesError(Exception):
+    """Describes the errors occurred when the number of lines from a triangle is
+    not a positive number.
     """
+
+    def __init__(self, num_lines):
+        if num_lines == 0:
+            self.msg = 'The triangle must have at least 1 line.'
+        elif num_lines < 0:
+            self.msg = 'Negative number of lines? Really? Try again.'
+
+    def __str__(self):
+        return self.msg
+
+class InvalidLimitsError(Exception):
+    """Describes the errors occurred when the number of limits are not correct.
+    """
+
+    def __init__(self, limits):
+        lim_min = limits[0]
+        lim_max = limits[1]
+        if lim_min == lim_max:
+            self.msg = 'No variability of values.'
+        elif lim_min > lim_max:
+            self.msg = 'How can you have an interval starting from a point higher\
+ than the end? Try again.'
+
+    def __str__(self):
+        return self.msg
+
+
+def create_example(num_lines, limits):
+    """Creates an example given the number of lines and the limits.
+    """
+    if num_lines < 1:
+        raise InvalidNumLinesError(num_lines)
+
+    lim_min = limits[0]
+    lim_max = limits[1]
+    if lim_min >= lim_max:
+        raise InvalidLimitsError(limits)
+
+    return [[rd.randint(lim_min, lim_max) for _ in range(i)] for i in range(1, num_lines + 1)]
+
+def print_result(example, path, total):
+    """Prints the maximum total in the form "X + Y + .... + Z = A".
+    """
+    print('path = %s' % path)
+    print('total = %d' % total)
+    if len(path) > 1:
+        for pos in path[: -1]:
+            print('%d + ' % example[pos[0]][pos[1]], end='')
+    print('%d = %d' % (example[path[-1][0]][path[-1][1]], total))
+
+
+def maximum_total(example, num_lines, path=[(0, 0)], total=None):
+    """Searches the "half matrix" for all paths and finds the better (higher sum).
+    In case of a tie, the position "down" has preference over "downright".
+
+    @param example: the triangle to be searched.
+    @param num_lines: the number of lines. It's passed to avoid call a function (len)
+    to calculate the number of lines (assuming the compiler will not optimize it).
+    @param path: the path that delivers the maximum total for a branch of example.
+    @param total: the maximum total for a branch of example.
+
+    @returns: a tuple containing path and total.
+    """
+    if len(path) == 1:
+        total = example[0][0]
+    if num_lines == 1:
+        return (path, total)
+
     i = path[-1][0] + 1
     j = path[-1][1]
     j_right = j + 1
 
     path_down = path + [(i, j)]
-    result_down = result + example[i][j]
+    total_down = total + example[i][j]
     path_downright = path + [(i, j_right)]
-    result_downright = result + example[i][j_right]
+    total_downright = total + example[i][j_right]
 
-    if i < size - 1:
-        (path_down, result_down) = find_result(example, size, path_down, result_down)
-        (path_downright, result_downright) = find_result(example, size, path_downright,
-                                                         result_downright)
+    if i < num_lines - 1:
+        (path_down, total_down) = maximum_total(example, num_lines, path_down, total_down)
+        (path_downright, total_downright) = maximum_total(example, num_lines, path_downright,
+                                                          total_downright)
 
-    if result_downright > result_down:
-        return (path_downright, result_downright)
+    if total_downright > total_down:
+        return (path_downright, total_downright)
     else:
-        return (path_down, result_down)
+        return (path_down, total_down)
 
 
 if __name__ == '__main__':
-    example = [[6], [3, 5], [9, 7, 1], [4, 6, 8, 4]]
-    (size, path, result) = (len(example), [(0, 0)], example[0][0])
-    (path, result) = find_result(example, size, path, result)
+    import sys
+    import time
 
-    print(path)
-    print(result)
-    print('%d + ' % example[path[0][0]][path[0][1]], end='')
-    for pos in path[1 : -1]:
-        print('%d + ' % example[pos[0]][pos[1]], end='')
-    print('%d = %d' % (example[path[-1][0]][path[-1][1]], result))
+    args = sys.argv[1 : ]
+    num_lines = int(args[0])
+    lim_min = int(args[1])
+    lim_max = int(args[2])
+
+    example = create_example(num_lines, (lim_min, lim_max))
+
+    t = time.time()
+    (path, result) = maximum_total(example, num_lines)
+    t = time.time() - t
+
+    print('example = %s' % example)
+    print_result(example, path, result)
+    print('In %f microseconds' % (t*1e6))
